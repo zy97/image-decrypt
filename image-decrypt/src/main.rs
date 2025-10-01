cc17adc072f22ead1709120f5a901b72437e8775
bomky <bom_ky@Outlook.com>
2025-10-01 17:33:54 +0800

    换成axum实现

--- image-decrypt/src/main.rs
+++ image-decrypt/src/main.rs
@@ -1,182 +1,145 @@
-use std::{
-    collections::HashMap,
-    fs,
-    sync::{Arc, Mutex},
-    time::Duration,
+use axum::http::{HeaderMap, HeaderName};
+use axum::response::IntoResponse;
+use axum::{
+    Router,
+    extract::{Query, State},
+    routing::get,
 };
-
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, get, web};
-use base64::{Engine as _, decode, engine::general_purpose};
-use deno_core::{JsRuntime, RuntimeOptions, url, v8::Local};
-use moka::future::Cache;
-use reqwest;
+use base64::{Engine, engine::general_purpose};
+use bb8_redis::{
+    RedisConnectionManager,
+    bb8::{self, Pool},
+    redis::AsyncCommands,
+};
+use deno_core::{JsRuntime, RuntimeOptions, v8::Local};
+use reqwest::header::CONTENT_TYPE;
+use reqwest::{self, StatusCode};
+use serde::Deserialize;
 use serde_v8::from_v8;
+use std::fs;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-const ONE_WEEK_IN_SECONDS: u64 = 60 * 60 * 24 * 7;
-async fn cached_endpoint(key: String, cache: web::Data<Cache<String, String>>) -> Option<String> {
-    // 尝试从缓存获取
-    cache.get(&key).await
-}
-async fn get_or_insert<F, Fut>(
-    key: String,
-    cache: web::Data<Cache<String, String>>,
-    setter: F,
-) -> Option<String>
-where
-    F: Fn(String) -> Fut + Send + Sync + 'static,
-    Fut: Future<Output = String> + Send + 'static,
-{
-    // 尝试从缓存获取
-    if let Some(data) = cache.get(&key).await {
-        return Some(data);
-    }
+async fn greet(query: Query<QueryInfo>, State(pool): State<ConnectionPool>) -> impl IntoResponse {
+    let mut conn = pool.get().await.map_err(internal_error)?;
 
-    // 如果缓存中没有数据，调用异步 setter 函数生成数据并插入缓存
-    let value = setter(key.clone()).await;
-    cache.insert(key, value.clone()).await;
-    Some(value)
-}
+    match query.0.image {
+        Some(image) => {
+            let image_cache: Option<String> = conn.get(&image).await.map_err(internal_error)?;
+            let image_base64 = match image_cache {
+                Some(image) => Some(image),
+                None => {
+                    // 获取图片内容
+                    let response = reqwest::get(&image).await.expect("Failed to fetch image");
 
-#[get("/")]
-async fn greet1(req: HttpRequest, cache: web::Data<Cache<String, String>>) -> impl Responder {
-    let query = req.query_string(); // 原始 query: "name=Tom&age=18"
-    let params: std::collections::HashMap<_, _> = url::form_urlencoded::parse(query.as_bytes())
-        .into_owned()
-        .collect();
+                    let image_data = response.bytes().await.expect("Failed to read image data");
+                    let image_data = image_data.to_vec();
+                    let image_data = general_purpose::STANDARD.encode(&image_data);
+                    let decrypt =
+                        fs::read_to_string("./decrypt.js").expect("Failed to read JS file");
+                    let crypto =
+                        fs::read_to_string("./crypto-js.js").expect("Failed to read JS file");
 
-    let image_url = params.get("image").unwrap();
-    let cache_data = cached_endpoint(image_url.to_string(), cache.clone()).await;
-    match cache_data {
-        Some(cached_image) => {
-            match decode(cached_image) {
-                Ok(image_bytes) => {
-                    HttpResponse::Ok()
-                        .content_type("image/png") // 或者 image/jpeg 等
-                        .body(image_bytes)
+                    let image_base64 = {
+                        // 创建 JS 运行时
+                        let mut runtime = JsRuntime::new(RuntimeOptions::default());
+
+                        // 加载 crypto-js
+                        runtime.execute_script("crypto-js", crypto).unwrap();
+                        runtime.execute_script("decrypt", decrypt).unwrap();
+
+                        let execute_js = format!(
+                            r#"
+                            decryptImage("{}");
+                        "#,
+                            image_data
+                        );
+                        let result = runtime
+                            .execute_script("test", execute_js)
+                            .expect("Failed to run script");
+
+                        let image_base64 = {
+                            let scope = &mut runtime.handle_scope(); // 创建作用域
+                            let local = Local::new(scope, &result);
+                            let value: String =
+                            from_v8(scope, local) // 转为 Rust 的 String
+                                .expect("Failed to convert result");
+                            value
+                        };
+                        image_base64
+                    };
+
+                    conn.set::<&String, String, ()>(&image, image_base64.clone())
+                        .await
+                        .unwrap();
+                    Some(image_base64)
                 }
-                Err(_) => HttpResponse::BadRequest().body("Invalid base64 string"),
-            }
-        }
-        None => {
-            // 获取图片内容
-            let response = reqwest::get(image_url)
-                .await
-                .expect("Failed to fetch image");
-            let image_data = response.bytes().await.expect("Failed to read image data");
-            let image_data = image_data.to_vec();
-            let image_data = general_purpose::STANDARD.encode(&image_data);
-            let decrypt = fs::read_to_string("./decrypt.js").expect("Failed to read JS file");
-            let crypto = fs::read_to_string("./crypto-js.js").expect("Failed to read JS file");
-            // 创建 JS 运行时
-            let mut runtime = JsRuntime::new(RuntimeOptions::default());
-
-            // 加载 crypto-js
-            runtime.execute_script("crypto-js", crypto).unwrap();
-            runtime.execute_script("decrypt", decrypt).unwrap();
-
-            let execute_js = format!(
-                r#"
-        decryptImage("{}");
-    "#,
-                image_data
-            );
-            // // 使用 JS 脚本中的函数，比如 CryptoJS.MD5
-            let result = runtime
-                .execute_script("test", execute_js)
-                .expect("Failed to run script");
-
-            let result_str = {
-                let scope = &mut runtime.handle_scope(); // 创建作用域
-                let local = Local::new(scope, &result);
-                let value: String = from_v8(scope, local) // 转为 Rust 的 String
-                    .expect("Failed to convert result");
-                value
             };
-            cache.insert(image_url.clone(), result_str.clone()).await;
-            match decode(result_str) {
-                Ok(image_bytes) => {
-                    HttpResponse::Ok()
-                        .content_type("image/png") // 或者 image/jpeg 等
-                        .body(image_bytes)
+            match image_base64 {
+                Some(image) => {
+                    let image_bytes = general_purpose::STANDARD.decode(image);
+                    match image_bytes {
+                        Ok(bytes) => {
+                            let mut headers = HeaderMap::new();
+                            headers.insert(CONTENT_TYPE, "image/png".parse().unwrap());
+                            return Ok((StatusCode::OK, headers, bytes));
+                        }
+                        Err(_) => {
+                            return Err((StatusCode::BAD_REQUEST, String::from("error image")));
+                        }
+                    }
                 }
-                Err(_) => HttpResponse::BadRequest().body("Invalid base64 string"),
-            }
-        }
-    }
-}
-#[get("/")]
-async fn greet(req: HttpRequest, cache: web::Data<Cache<String, String>>) -> impl Responder {
-    let query = req.query_string(); // 原始 query: "name=Tom&age=18"
-    let params: std::collections::HashMap<_, _> = url::form_urlencoded::parse(query.as_bytes())
-        .into_owned()
-        .collect();
-
-    let image_url = params.get("image").unwrap().clone();
-    let data = get_or_insert(image_url.clone(), cache, async move |key| {
-        // 获取图片内容
-        let response = reqwest::get(key).await.expect("Failed to fetch image");
-        let image_data = response.bytes().await.expect("Failed to read image data");
-        let image_data = image_data.to_vec();
-        let image_data = general_purpose::STANDARD.encode(&image_data);
-        let decrypt = fs::read_to_string("./decrypt.js").expect("Failed to read JS file");
-        let crypto = fs::read_to_string("./crypto-js.js").expect("Failed to read JS file");
-        // 创建 JS 运行时
-        let mut runtime = JsRuntime::new(RuntimeOptions::default());
-
-        // 加载 crypto-js
-        runtime.execute_script("crypto-js", crypto).unwrap();
-        runtime.execute_script("decrypt", decrypt).unwrap();
-
-        let execute_js = format!(
-            r#"
-        decryptImage("{}");
-    "#,
-            image_data
-        );
-        // // 使用 JS 脚本中的函数，比如 CryptoJS.MD5
-        let result = runtime
-            .execute_script("test", execute_js)
-            .expect("Failed to run script");
-
-        let result_str = {
-            let scope = &mut runtime.handle_scope(); // 创建作用域
-            let local = Local::new(scope, &result);
-            let value: String = from_v8(scope, local) // 转为 Rust 的 String
-                .expect("Failed to convert result");
-            value
-        };
-        result_str
-    })
-    .await;
-    match data {
-        Some(cached_image) => {
-            match decode(cached_image) {
-                Ok(image_bytes) => {
-                    HttpResponse::Ok()
-                        .content_type("image/png") // 或者 image/jpeg 等
-                        .body(image_bytes)
-                }
-                Err(_) => HttpResponse::BadRequest().body("Invalid base64 string"),
+                None => return Err((StatusCode::BAD_REQUEST, String::from("no image"))),
             }
         }
         None => {
-            return HttpResponse::NotFound().body("Image not found in cache or failed to fetch");
+            return Err((StatusCode::BAD_REQUEST, String::from("no image")));
         }
     }
+
+    // return (StatusCode::BAD_REQUEST, String::new());
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    // 创建缓存，设置一周有效期
-    let cache: Cache<String, String> = Cache::builder()
-        .time_to_live(Duration::from_secs(ONE_WEEK_IN_SECONDS))
-        .build();
-    HttpServer::new(move || {
-        App::new()
-            .app_data(web::Data::new(cache.clone()))
-            .service(greet)
-    })
-    .bind(("0.0.0.0", 9090))?
-    .run()
-    .await
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| format!("{}=debug", env!("CARGO_CRATE_NAME")).into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    tracing::debug!("connecting to redis");
+    let manager = RedisConnectionManager::new("redis://localhost").unwrap();
+    let pool = bb8::Pool::builder().build(manager).await.unwrap();
+
+    {
+        // ping the database before starting
+        let mut conn = pool.get().await.unwrap();
+        conn.set::<&str, &str, ()>("foo", "bar").await.unwrap();
+        let result: String = conn.get("foo").await.unwrap();
+        assert_eq!(result, "bar");
+    }
+    tracing::debug!("successfully connected to redis and pinged it");
+
+    // build our application with a route
+    let app = Router::new().route("/", get(greet)).with_state(pool);
+
+    // run it
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:9090").await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+#[derive(Deserialize)]
+struct QueryInfo {
+    image: Option<String>,
+}
+
+type ConnectionPool = Pool<RedisConnectionManager>;
+
+fn internal_error<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
